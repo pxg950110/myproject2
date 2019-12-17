c9339e70b5912054c8e3963c3344c9dcbe3d3f76
pxg950110 <pxg950110@163.com>
2019-12-17 23:18:49 +0800

    android

---
+++ app/src/main/java/com/pxg/myproject2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.pxg.myproject2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author pxg
+ * @emial pxg950110@163.com
+ * @see android.app.Activity
+ * @deprecated
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
